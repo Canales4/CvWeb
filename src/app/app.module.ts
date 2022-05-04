8ca38b415bcc89dae8fc0d1e68bd1dd6a923a3c7
Canales4 <canaleslinaresjuancarlos@gmail.com>
2022-05-04 09:43:37 +0200

    demo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { AboutComponent } from './about/about.component';
+import { ExpirienceComponent } from './expirience/expirience.component';
+import { StudiesComponent } from './studies/studies.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainpageComponent,
+    NavbarComponent,
+    AboutComponent,
+    ExpirienceComponent,
+    StudiesComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
