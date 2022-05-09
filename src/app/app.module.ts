e55fa302673b1370ba3d9ec16864fabb0a6dfaf2
Juan Carlos Canales <canaleslinaresjuancarlos@gmail.com>
2022-05-09 20:40:05 +0200

    Traduccion pagina y ultimos retoques 9 mayo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { AboutComponent } from './about/about.component';
 import { ExpirienceComponent } from './expirience/expirience.component';
 import { StudiesComponent } from './studies/studies.component';
 import { ContactComponent } from './contact/contact.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -26,9 +29,22 @@ import { ContactComponent } from './contact/contact.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
+export function translateFactory(httpClient: HttpClient){
+  return new TranslateHttpLoader(httpClient);
+}
